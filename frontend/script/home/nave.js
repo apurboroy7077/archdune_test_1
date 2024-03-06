4d2efcc777e0101d0f1c57d7775f147db2a7df31
Apurbo Roy <apurboroy7077@gmail.com>
2024-03-06 08:49:36 +0600

    Fixed Navbar Icon Dissapear Problem.

--- frontend/script/home/nave.js
+++ frontend/script/home/nave.js
@@ -3,25 +3,30 @@ const allHadItem = document.querySelector(".header-item");
 const mobileNavbarBtn = document.querySelector(".mobile-navbar-btn");
 
 mobileNavbarBbtn.addEventListener("click", () => {
-    allHadItem.classList.toggle("h-active");
-    mobileNavbarBtn.classList.toggle("h-active");
+  allHadItem.classList.toggle("h-active");
+  mobileNavbarBtn.classList.toggle("h-active");
 });
 
-const header = document.querySelector(".navBar");
+const header = document.querySelector(".project_page_header");
 // const mainBar = document.querySelector("main");
 
-window.addEventListener('scroll', function() {
-    header.classList.toggle("sticky", window.scrollY > 150)
-})
+window.addEventListener("scroll", function () {
+  header.classList.toggle(
+    "project_page_header_while_scroll",
+    window.scrollY > 150
+  );
+});
 
 const active_page = window.location.pathname;
 const navLinks = document.querySelectorAll(".all-had-item a");
 
-
-navLinks.forEach(link => {
-    if ((link.pathname === '/' && active_page === '/index.html') || link.pathname === active_page) {
-        link.classList.add("active");
-    } else {
-        link.classList.remove("active");
-    }
-})
+navLinks.forEach((link) => {
+  if (
+    (link.pathname === "/" && active_page === "/index.html") ||
+    link.pathname === active_page
+  ) {
+    link.classList.add("active");
+  } else {
+    link.classList.remove("active");
+  }
+});
