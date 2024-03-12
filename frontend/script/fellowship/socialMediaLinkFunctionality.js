77073aec16aefcfe320fd69522ea08b9c8df7081
Apurbo Roy <apurboroy7077@gmail.com>
2024-03-12 19:56:36 +0600

    Added link functionality in fellowship page's social media section's button

---
+++ frontend/script/fellowship/socialMediaLinkFunctionality.js
@@ -0,0 +1,24 @@
+let facebookButton = document.getElementsByClassName(
+  "facebook-button-in-fellowship"
+)[0];
+let twitterButton = document.getElementsByClassName(
+  "twitter-button-in-fellowship"
+)[0];
+let youtubeButton = document.getElementsByClassName(
+  "youtube-button-in-fellowship"
+)[0];
+let pinterestButton = document.getElementsByClassName(
+  "pinterest-button-in-fellowship"
+)[0];
+facebookButton.addEventListener("click", () => {
+  window.open("https://facebook.com", "_blank");
+});
+twitterButton.addEventListener("click", () => {
+  window.open("https://twitter.com", "_blank");
+});
+youtubeButton.addEventListener("click", () => {
+  window.open("https://youtube.com", "_blank");
+});
+pinterestButton.addEventListener("click", () => {
+  window.open("https://pinterest.com", "_blank");
+});
