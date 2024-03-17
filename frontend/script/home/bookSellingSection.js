54e312126711ddd8fb517e41d726f6e4d9aef14d
Apurbo Roy <apurboroy7077@gmail.com>
2024-03-17 20:24:42 +0600

    Added Book Section Prototype.

---
+++ frontend/script/home/bookSellingSection.js
@@ -0,0 +1,30 @@
+let imagesSrc = [
+  "./images/book_section/chashi bari_01.jpeg",
+  "./images/book_section/chashi bari_02.jpeg",
+  "./images/book_section/chashi bari_03.jpeg",
+  "./images/book_section/chashi bari_04.jpeg",
+  "./images/book_section/chashi bari_05.jpeg",
+  "./images/book_section/chashi bari_06.jpeg",
+];
+let sliderDiv = document.getElementsByClassName(
+  "book-selling-section-swipper-wrapper"
+)[0];
+let loadImagesInDiv = () => {
+  let code = ``;
+  for (let i = 0; i < imagesSrc.length; i++) {
+    let imageSrc = imagesSrc[i];
+    let singleCode = ` <div class="swiper-slide book-selling-section-image-div">
+    <img
+      src="${imageSrc}"
+      class="book-selling-section-image"
+    />
+  </div>`;
+    code = code + singleCode;
+  }
+  sliderDiv.innerHTML = code;
+};
+try {
+  loadImagesInDiv();
+} catch (error) {
+  console.log(error);
+}
