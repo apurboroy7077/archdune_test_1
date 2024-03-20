b58aeaf1628a8180f84ac41de8e13dbc5d229cae
Apurbo Roy <apurboroy7077@gmail.com>
2024-03-20 10:36:31 +0600

    book_section_style_1

--- frontend/script/home/bookSellingSection.js
+++ frontend/script/home/bookSellingSection.js
@@ -1,10 +1,10 @@
 let imagesSrc = [
-  "./images/book_section/chashi bari_01.jpg",
-  "./images/book_section/chashi bari_04.jpg",
-  "./images/book_section/chashi bari_05.jpg",
-  "./images/book_section/chashi bari_06.jpg",
-  "./images/book_section/chashi bari_02.jpg",
-  "./images/book_section/chashi bari_03.jpg",
+  "./images/book_section_2/chashi bari_01.jpg",
+  "./images/book_section_2/chashi bari_02.jpg",
+  "./images/book_section_2/chashi bari_03.jpg",
+  "./images/book_section_2/chashi bari_04.jpg",
+  "./images/book_section_2/chashi bari_05.jpg",
+  "./images/book_section_2/chashi bari_06.jpg",
 ];
 let sliderDiv = document.getElementsByClassName(
   "book-selling-section-swipper-wrapper"
@@ -13,11 +13,15 @@ let loadImagesInDiv = () => {
   let code = ``;
   for (let i = 0; i < imagesSrc.length; i++) {
     let imageSrc = imagesSrc[i];
-    let singleCode = ` <div class="swiper-slide book-selling-section-image-div">
+    let singleCode = ` <div class="swiper-slide ">
+    <div style="width:100%; overflow:hidden; border-radius:5px">
     <img
       src="${imageSrc}"
-      class="book-selling-section-image"
+      class="hover-scale-1-1 transition-0-5s-ease"
+      style="width:100%"
     />
+    </div>
+    
   </div>`;
     code = code + singleCode;
   }
