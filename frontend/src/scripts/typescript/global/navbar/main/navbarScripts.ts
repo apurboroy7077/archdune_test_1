8edaa352550392f9779673e853a27fa145ababa2
Apurbo Roy <apurboroy7077@gmail.com>
2024-09-26 19:15:19 +0600

    competition page added

---
+++ frontend/src/scripts/typescript/global/navbar/main/navbarScripts.ts
@@ -0,0 +1,13 @@
+import closeMenubarInSmallScreenFunctionality from "../close-menu-list-in-small-screen-functionality/closeMenubarInSmallScreenFunctionality.js";
+import linkFunctionOfBigScreenNavbar from "../link-function-of-big-screen-navbar/linkFunctionOfBigScreenNavbar.js";
+import openNavbarMenuListInSmallScreenFunctionality from "../open-navbar-menu-list-in-small-screen/openNavbarMenuListInSmallScreenFunctionality.js";
+import specialLinkFunction from "../special-link-function/specialLinkFunction.js";
+
+const navbarScripts = () => {
+  openNavbarMenuListInSmallScreenFunctionality();
+  closeMenubarInSmallScreenFunctionality();
+  linkFunctionOfBigScreenNavbar();
+  specialLinkFunction();
+};
+
+export default navbarScripts;
