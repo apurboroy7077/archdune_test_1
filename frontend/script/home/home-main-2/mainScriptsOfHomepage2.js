fccef3e8374f8a229fedcced76f670e37872510e
Apurbo Roy <apurboroy7077@gmail.com>
2024-10-27 17:45:10 +0600

    replaced header in project page

---
+++ frontend/script/home/home-main-2/mainScriptsOfHomepage2.js
@@ -0,0 +1,6 @@
+import navbarScripts from "../../../src/scripts/javascript/global/navbar/main/navbarScripts.js";
+
+const mainScriptsOfHomepage2 = () => {
+  navbarScripts();
+};
+mainScriptsOfHomepage2();
